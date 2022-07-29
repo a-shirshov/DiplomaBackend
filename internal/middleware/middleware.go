bade302debb989f85c74b4928843e47e4bc02b92
Artyom Shirshov <artyomsh01@yandex.ru>
2022-07-29 21:25:59 +0300

    Cors fix

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,7 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"Diploma/utils"
 	"net/http"
+	"log"
 )
+
+var allowedOrigins = []string{"http://45.141.102.243:8080", "http://127.0.0.1:8080"}
+
 
 func TokenAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -20,7 +24,21 @@ func TokenAuthMiddleware() gin.HandlerFunc {
 
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := c.Request.Header.Get("Origin")
+		isAllowed := false
+		for _, orig := range allowedOrigins {
+			if origin == orig {
+				isAllowed = true
+				return
+			}
+		}
+
+		if !isAllowed {
+			log.Print("CORS not allowed origin = ", origin)
+			return
+		}
+
+        c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
