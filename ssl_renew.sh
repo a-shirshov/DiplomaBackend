5cbd20d58bb0e3b283bc8c3baa52eb2c1bfaf7ff
Artyom Shirshov <artyomsh01@yandex.ru>
2022-08-29 13:52:28 +0300

    ssl-try

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 _#!/bin/bash
 
 /usr/local/bin/docker-compose -f /root/DiplomaBackend/docker-compose.yml run certbot renew --dry-run \
-&& /usr/local/bin/docker-compose -f /root/DiplomaBackend/docker-compose.yml kill -s SIGHUP webserver
+&& /usr/local/bin/docker-compose -f /root/DiplomaBackend/docker-compose.yml kill -s SIGHUP nginx
