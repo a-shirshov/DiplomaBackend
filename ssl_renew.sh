ef5629b183bf660a7fbf08e5dec364a283a7f0fa
Artyom Shirshov <artyomsh01@yandex.ru>
2022-09-14 12:30:55 +0300

    test cron

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/docker-compose -f /root/DiplomaBackend/docker-compose.yml run certbot renew \
-&& /usr/bin/docker-compose -f /root/DiplomaBackend/docker-compose.yml kill -s SIGHUP nginx
+/usr/bin/docker-compose -f /home/artyom/DiplomaBackend/docker-compose.yml run certbot renew --dry-run \
+&& /usr/bin/docker-compose -f /home/artyom/DiplomaBackend/docker-compose.yml kill -s SIGHUP nginx
