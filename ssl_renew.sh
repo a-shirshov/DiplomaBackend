dbaa216f484678f5b64c51d70b6c44b84494b8ff
Artyom Shirshov <artyomsh01@yandex.ru>
2022-09-16 18:25:05 +0300

    ssl_fix

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/docker-compose -f /home/artyom/DiplomaBackend/docker-compose.yml run --no-deps certbot renew --dry-run \
+/usr/bin/docker-compose -f /home/artyom/DiplomaBackend/docker-compose.yml --env-file /home/artyom/DiplomaBackend/.env run --no-deps certbot renew --dry-run \
 && /usr/bin/docker-compose -f /home/artyom/DiplomaBackend/docker-compose.yml kill -s SIGHUP nginx
