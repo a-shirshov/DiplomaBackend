ed4b673bd7a6d8b6631472f48fa89a8b0454bad2
Artyom Shirshov <artyomsh01@yandex.ru>
2023-05-05 13:51:38 +0300

    test

--- recomendationModule/updater.py
+++ recomendationModule/updater.py
@@ -101,10 +101,11 @@ def get_future_events():
     place_list = []
     print(unix_timestamp_tomorrow_start)
     # создаем URL с параметром actual_since
-    event_url = f'https://kudago.com/public-api/v1.4/events/?fields=id,dates,title,images,location,place,description,price&actual_since={unix_timestamp_tomorrow_start}&page_size=50'
+    event_url = f'https://kudago.com/public-api/v1.4/events/?fields=id,dates,title,images,location,place,description,price&actual_since={unix_timestamp_tomorrow_start}&page_size=25'
     # отправляем GET-запрос и получаем ответ в формате JSON
     response = requests.get(event_url)
     json_data_events = json.loads(response.text)
+    i=0
     while True:
         for data_event in json_data_events['results']:
             #print(data_event)
@@ -130,7 +131,9 @@ def get_future_events():
                 place_list.append(place)
             except Exception:
                 pass
-
+        if i > 2:
+            break
+        i=i+1
         if json_data_events["next"] == None:
             break
 
